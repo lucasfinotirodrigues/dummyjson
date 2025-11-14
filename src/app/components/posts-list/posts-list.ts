644f614c1028924caccc4f642cef1dc57d128f4b
Lucas Finoti Rodrigues <lucas.rodrigues@ciag.org.br>
2025-11-14 15:03:36 -0300

    feat: add Bootstrap and ng-bootstrap for modal functionality

--- src/app/components/posts-list/posts-list.ts
+++ src/app/components/posts-list/posts-list.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PostsCreate } from '../posts-create/posts-create';
+import { PostsUpdate } from '../posts-update/posts-update';
+import { PostsDelete } from '../posts-delete/posts-delete';
 
 @Component({
   selector: 'app-posts-list',
@@ -7,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrl: './posts-list.scss',
 })
 export class PostsList {
+  private readonly modalService = inject(NgbModal);
 
+  openModalCreate() {
+    this.modalService.open(PostsCreate, { size: 'lg' });
+  }
+
+  openModalUpdate() {
+    this.modalService.open(PostsUpdate, { size: 'lg' });
+  }
+
+  openModalDelete() {
+     this.modalService.open(PostsDelete, { size: 'lg' });
+  }
 }
